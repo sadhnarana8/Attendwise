f4f501b835cc87d45ead69f4134f059161a04e48
sadhnarana8 <111896255+sadhnarana8@users.noreply.github.com>
2023-08-20 16:21:53 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,392 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="Description" content="Enter your description here" />
+  <title>ATTENDWISE</title>
+
+
+  <!-- bootstrap CDN -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+
+  <!-- data aos animtion -->
+  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+  <!-- owl carousel -->
+  <link rel="stylesheet" href="css/owl.carousel.css">
+  <link rel="stylesheet" href="css/owl.theme.default.css">
+
+
+  <!-- OWN CSS -->
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/responsive-style.css">
+  
+  <link rel="icon" href="images/attendwise.png" type="image/png">
+  
+</head>
+
+<body>
+
+  <!-- Header -->
+  <header id="header">
+    <!-- Top Navbar -->
+    <nav class="navbar navbar-expand top d-none d-lg-block d-md-block pt-md-2">
+      <div class="container header">
+
+        <!-- Navbar Itesm [left] -->
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a href="#" class="nav-link smooth-anchor pl-0"><i class="fas fa-clock mr-2"></i>Your attendance shapes your future; be there and make it count</a>
+          </li>
+        </ul>
+
+        <div class="ml-auto"></div>
+
+        <!-- Navbar Items [Right] -->
+        <ul class="navbar-nav">
+          <li class="nav-item mr-3">
+            <a href="#" class="nav-link"><i class="fas fa-phone-alt mr-2"></i>+91 7206182169</a>
+          </li>
+          <li class="nav-item mr-3">
+            <a href="#" class="nav-link"><i class="fas fa-envelope mr-2"></i>chaitanyakim2003@gmail.com</a>
+          </li>
+        </ul>
+
+        <!-- Navbar Icons -->
+        <ul class="navbar-nav icons">
+          <li class="nav-item social mr-3">
+            <a href="#" class="nav-link"><i class="fab fa-facebook-f"></i></a>
+          </li>
+          <li class="nav-item social mr-3">
+            <a href="#" class="nav-link"><i class="fab fa-twitter"></i></a>
+          </li>
+          <li class="nav-item social mr-3">
+            <a href="#" class="nav-link pr-0"><i class="fab fa-linkedin-in"></i></a>
+          </li>
+        </ul>
+
+      </div>
+    </nav>
+
+    <!-- Main Header -->
+    <nav class="navbar navbar-expand-lg navigation-wrap">
+      <div class="container">
+        <a class="navbar-brand" href="#"><img decoding="async" src="images/attendwise-logo.png"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText"
+          aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <i class="fas fa-stream navbar-toggler-icon"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+          <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+            <li class="nav-item active">
+              <a class="nav-link" aria-current="page" href="#home">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#about">About Us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#approach">Our Services </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#team">Team</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav> 
+  </header>
+   
+  <!-- Section-1 banner design -->
+  <section id="home">
+    <div class="main-wrapper">
+      <div class="top-wrapper">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-6 pr-lg-5 pr-md-5 pr-2 left-wrapper">
+              <h5 class="py-4"></h5>
+              <h1 data-aos="zoom-in" data-aos-delay="800">Attend,  <span> achieve,  </span> inspire.</h1>
+              <p data-aos="zoom-in" data-aos-delay="800">Access real-time attendance tracking and stay on top of your academic journey with our student-friendly open-source portal.</p>
+			  
+              <div data-aos="fade-up" data-aos-delay="600" class="mt-5"">
+                <a href="#contact" class="smooth-anchor main-btn">PORTAL ENTRY</a>
+              </div>
+			 
+            </div>
+            <div class="col-lg-6 right-wrapper">
+              <div data-aos="fade-left" data-aos-delay="500">
+                <img decoding="async" src="images/sadhna-1.png" alt="lifecoach">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <div class="clearfix"></div>
+
+  <!-- Section-2 about design -->
+  <section id="about">
+    <div class="wrapper about">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 col-md-12 left-wrapper">
+            <div class="about-img position-relative" data-aos="zoom-in" data-aos-delay="800">
+              <img decoding="async" src="images/about-img/about-img.png" alt="about" class="img-fluid">
+            </div>
+          </div>
+          <div class="col-lg-6 col-md-12 pt-md-5 right-wrapper">
+            <div data-aos="fade-left" data-aos-delay="500">
+              <h5>About Our Portal</h5>
+                <h2>Unleashing the Potential of Attendance Management</h2>
+                <p>We are passionate about transforming the way educational institutions handle attendance tracking. Our cutting-edge portal offers a user-friendly interface, empowering students and administrators to easily monitor and manage attendance records.</p>
+                <p> Our innovative attendance portal is designed to empower students and administrators alike, making attendance tracking a seamless and efficient process.Our mission is to revolutionize attendance tracking, providing a seamless experience that saves time and enhances efficiency.
+                </p>
+            </div>
+            <div class="mt-5" data-aos="fade-up" data-aos-delay="500">
+              <a href="newSiteHigh/new.html" class="main-btn">Learn More</a>
+            </div>
+          </div>
+        </div> 
+      </div>
+    </div>
+  </section>
+  <div class="clearfix"></div>
+
+  <!-- Section-3 our services design -->
+  <section id="approach">
+    <div class="wrapper approach">
+      <div class="container">
+        <div class="heading text-center">
+          
+          <h2><span>Our Services</span></h2>
+        </div>
+         <div class="pt-5 pb-5">
+    <div class="container" > 
+      <div class="row">
+        
+        <div class="col-lg-4 col-sm-6">
+          <div class="item"> <span class="icon feature_box_col_one"><i class="fa fa-clock"></i></span>
+            <h6>Real-time Attendance Tracking</h6>
+            <p>Our attendance portal enables seamless and accurate tracking of attendance, allowing students and administrators to stay updated on attendance records in real-time.</p>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <div class="item"> <span class="icon feature_box_col_two"><i class="fa fa-anchor"></i></span>
+            <h6>Notifications and Reminders</h6>
+            <p>Our portal sends automated notifications and reminders to students, reminding them of upcoming classes, deadlines, or any changes in the schedule to ensure they never miss a session.</p>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-6 ">
+          <div class="item"> <span class="icon feature_box_col_three"><i class="fa fa-hourglass-half"></i></span>
+            <h6>Analytics and Reporting</h6>
+            <p> Our attendance portal provides comprehensive analytics and reporting features, allowing administrators to generate insightful reports on attendance trends, patterns, and overall performance.</p>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <div class="item"> <span class="icon feature_box_col_four"><i class="fa fa-database"></i></span>
+            <h6>Student Self-Service</h6>
+            <p>Students have access to their attendance records, allowing them to monitor their attendance progress, identify areas for improvement, and take proactive steps to enhance their academic journey.</p>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <div class="item"> <span class="icon feature_box_col_five"><i class="fa fa-upload"></i></span>
+            <h6>Integration Capabilities</h6>
+            <p>Our attendance portal seamlessly integrates with existing student management systems, enabling smooth data synchronization and minimizing manual data entry for administrators.</p>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <div class="item"> <span class="icon feature_box_col_six"><i class="fa fa-camera"></i></span>
+            <h6>Customization and Flexibility</h6>
+            <p>Our system provides customizable attendance settings to accommodate various class structures & scheduling needs.
+			</p>
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
+      </div>
+    </div>
+  </section>
+  <div class="clearfix"></div>
+
+  <!-- Section-4 team design -->
+  <section id="team">
+    <div class="wrapper">
+      <div class="container">
+        <div class="heading text-center">
+        
+          <h2><span>Our Team</span></h2>
+        </div> 
+      </div>
+        
+	   <div class="container">
+	   <div class = "content-s">
+	 
+        <div class="card">
+            <div class="content">
+                <div class="imgBx">
+                    <img src="images/user-1.jpg" alt="">
+                </div>
+                <div class="contentBx">
+                    <h4>Chaitanya</h4>
+                    <h5>Web Designer</h5>
+                </div>
+                <div class="sci">
+                    <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                    <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                    <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="content">
+                <div class="imgBx">
+                    <img src="images/user-2.jpg" alt="">
+                </div>
+                <div class="contentBx">
+                    <h4>Sadhna</h4>
+                    <h5>Web Designer</h5>
+                </div>
+                <div class="sci">
+                    <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                    <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                    <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="content">
+                <div class="imgBx">
+                    <img src="images/user-3.jpg" alt="">
+                </div>
+                <div class="contentBx">
+                    <h4>Rukmani</h4>
+                    <h5>Web Designer</h5>
+                </div>
+                <div class="sci">
+                    <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                    <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                    <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+	</div>
+   	
+	</div>
+  </section>
+  <div class="clearfix"></div>  
+
+  <!-- Section-5 contact design -->
+  <section id="contact">
+    <div class="wrapper contact">
+	<div class="cont">
+      <div class="container">
+        <div class="heading text-center">
+          <h5></h5>
+          <h2>Contact here</h2>
+        </div>
+      </div>
+      
+<div class="container">
+<div class="content-c">
+<div class="image-box">
+<img src="images/contact.png" alt="">
+</div>
+<form action="#" onsubmit=" sendEmail(); reset(); return false">
+<div class="topic">Send us a message</div>
+<div class="input-box">
+<input type="text" required>
+<label>Enter your name</label>
+</div>
+<div class="input-box">
+<input type="text" required>
+<label>Enter your email</label>
+</div>
+<div class="input-box">
+<input type="text" required>
+<label>Enter your message</label>
+</div>
+<div class="input-box">
+<input type="submit" value="Send Message">
+</div>
+</form>
+</div>
+</div>
+
+
+
+
+
+</div>
+
+
+
+
+    </div>
+  </section>
+  <div class="clearfix"></div>  
+
+  <!-- Section-6 footer -->
+  <footer>
+      <div class="p-3 copyright" id="copyright">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-12 col-md-6 p-3 text-center text-lg-left mob-pb-0">
+              <p class="my-0">Copyright © 2023 <a href="#">Attendance Portal</a> All Rights Reserved</p>
+            </div>
+            <div class="col-12 col-md-6 p-3 text-center text-lg-right mob-pt-0">
+              <p>Designed by <a href="#" target=" _blank">Chaitanya</a>.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+  </footer>
+
+  <!-- JS Libraries -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
+
+  <!-- animation js -->
+  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+  <!-- owl carousel -->
+  <script src="js/owl.carousel.js"></script>
+
+  <!-- own js -->
+  <script src="js/main.js"></script>
+   
+<!-- smtpjs -->
+<script src=" https://smtpjs.com/v3/smtp.js"></script>
+<script>
+    function sendEmail(){
+     Email.send({
+    Host : "smtp.gmail.com",
+    Username : "chaitanyakim2003@gmailcom",
+    Password : "password",
+    To : 'chaitanyakim2021@gmail.com',
+    From : document.getElementById("email").value,,
+    Subject : "New Contact Form Enquiry",
+    Body : "Name:" + document.getElementById("name").value
+     +"<br> Email"  + document.getElementById("email").value
+}).then(
+  message => alert(message)
+);
+
+
+}
+</script>
+
+</body>
+
+</html>
+
